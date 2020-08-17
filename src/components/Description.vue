57841c3515df0679313c6da7842e8d73f370cd78
ytygrunn1ngark456 <ygrunn1ngark456@gmail.com>
2020-08-17 01:25:58 +0000

    Initial commit

---
+++ src/components/Description.vue
@@ -0,0 +1,10 @@
+<style lang="stylus" scoped>
+    h2
+        color #fff;
+</style>
+
+<template>
+    <div class="description">
+       <h2>A Simple Online Logo Generator</h2>
+    </div>
+</template>
